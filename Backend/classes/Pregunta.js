7971e11630868e62b285fba9a92fff294f970f87
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2022-05-31 20:40:15 +0200

    Update Pregunta.js

--- Backend/classes/Pregunta.js
+++ Backend/classes/Pregunta.js
@@ -1,7 +1,9 @@
 'use strict'
+import pool from '../db.js';
+
 export default class Pregunta {
 
-    constructor(pregunta = null, opciones = [], respuestaCorrecta = null, inforRelevante = null, dificultad = null) {
+    constructor(pregunta = null, opciones = [], respuestaCorrecta = null, inforRelevante = null, dificultad = null, id, respuesta) {
         if (pregunta && opciones && respuestaCorrecta && inforRelevante && dificultad) {
             this.pregunta = pregunta;
             this.opciones = opciones;
@@ -9,8 +11,39 @@ export default class Pregunta {
             this.inforRelevante = inforRelevante;
             this.dificultad = dificultad;
         }
+        if(id && respuesta){
+            this.id = id;
+            this.respuesta = respuesta;
+        }
 
     }
+    
+
+    validarPregunta(callback) {
+        let sql = "SELECT * FROM preguntas WHERE id = ? AND respuestaCorrecta = ?";
+        pool.query(sql, [this.id, this.respuesta], (err, rows) => {
+            if (err) {
+                console.log(err);
+                callback({
+                    status:false,
+                    protocol:"err",
+                    message:"Hubo un error con la base de datos"
+                })
+            }else if(rows.length <=0){
+                callback({
+                    status:false,
+                    protocol:"err",
+                    message:"La respuesta es incorrecta"
+                })
+            }else{
+                callback({
+                    status:true,
+                    protocol:"success"
+                })
+            }
+        })
+    }
+
     getRespuestaCorrecta() {
         return this.respuestaCorrecta;
     }
@@ -39,8 +72,8 @@ export default class Pregunta {
         return this.id;
     }
 
-    setId(id){
-        this.id=id;
+    setId(id) {
+        this.id = id;
     }
 
 }
