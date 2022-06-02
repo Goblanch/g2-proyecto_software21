24f2e9fe68c7ef14e1730abb46b2c89fef6a00b0
Ivanlpc <100932340+Ivanlpc@users.noreply.github.com>
2022-06-02 19:19:15 +0200

    Update preguntas.js

--- Backend/controller/preguntas.js
+++ Backend/controller/preguntas.js
@@ -7,41 +7,43 @@ import Usuario from "../classes/Usuario.js";
 const PreguntaController = {
     AddPregunta: (req, res) => {
         const { pregunta, opciones, respuestaCorrecta, inforRelevante, dificultad, token } = req.body;
+        
         const a = new Auth();
         a.Admin(token, (esAdmin) => {
             if (esAdmin.status) {
                 let a = new Admin();
 
                 if (pregunta && opciones && respuestaCorrecta && inforRelevante && dificultad) {
-                    const p = new Pregunta(pregunta, opciones, respuestaCorrecta, inforRelevante, dificultad);
+                    const p = new Pregunta(pregunta, JSON.stringify(opciones), respuestaCorrecta, inforRelevante, dificultad);
                     a.addPregunta(p, (respuesta) => {
-                        if(respuesta.status){
+                        if (respuesta.status) {
                             res.status(200).send({
-                                status:true,
-                                protocol:"success",
-                                message:"Pregunta añadida"
+                                status: true,
+                                protocol: "success",
+                                message: "Pregunta añadida",
+                                id: respuesta.data
                             })
-                        }else{
+                        } else {
                             res.status(300).send({
-                                status:false,
-                                protocol:"err",
-                                message:respuesta.message
+                                status: false,
+                                protocol: "err",
+                                message: respuesta.message
                             })
                         }
                     });
-                }else{
+                } else {
                     res.status(300).send({
-                        status:false,
-                        protocol:"err",
-                        message:"Debe rellenar todos los campos"
+                        status: false,
+                        protocol: "err",
+                        message: "Debe rellenar todos los campos"
                     });
                 }
 
-            }else{
+            } else {
                 res.status(300).send({
-                    status:false,
-                    protocol:"err",
-                    message:"El usuario no es administrador"
+                    status: false,
+                    protocol: "err",
+                    message: "El usuario no es administrador"
                 });
             }
         })
@@ -81,24 +83,24 @@ const PreguntaController = {
         })
 
     },
-    ConstruirNivel: (req, res)=> {
-        const {token} = req.headers;
+    ConstruirNivel: (req, res) => {
+        const { token } = req.headers;
         const a = new Auth();
         a.Validate(token, (respuesta) => {
             if (respuesta.status) {
                 const n = new Nivel(3);
-                setTimeout(()=> { //Tiempo de espera entes de enviar la respuesta para que la promesa se pueda cumplir
+                setTimeout(() => { //Tiempo de espera entes de enviar la respuesta para que la promesa se pueda cumplir
                     res.status(200).send({
-                        status:true,
-                        protocol:"success",
-                        data:n.getNivelDiario()
+                        status: true,
+                        protocol: "success",
+                        data: n.getNivelDiario()
                     })
                 }, 500);
 
-            const u = new Usuario();
-            u.intentoDiario(token);
-                
-            }else{
+                const u = new Usuario();
+                u.intentoDiario(token);
+
+            } else {
                 res.status(500).send({
                     status: false,
                     protocol: "err",
@@ -111,24 +113,26 @@ const PreguntaController = {
         const { id, token, respuestaUsuario } = req.body;
         const a = new Auth();
         a.Validate(token, (respuesta) => {
-            if(respuesta.status) {
+            if (respuesta.status) {
                 const p = new Pregunta(null, null, null, null, null, id, respuestaUsuario);
                 p.validarPregunta((correcta) => {
-                    if(correcta.status){
+                    if (correcta.status) {
+                        const u = new Usuario();
+                        u.sumarAcierto(token);
                         res.status(200).send({
-                            status:true,
-                            protocol:"success",
-                            message:"Respuesta correcta"
+                            status: true,
+                            protocol: "success",
+                            message: "Respuesta correcta"
                         })
-                    }else{
+                    } else {
                         res.status(300).send({
-                            status:false,
-                            protocol:"err",
-                            message:correcta.message
+                            status: false,
+                            protocol: "err",
+                            message: correcta.message
                         })
                     }
                 })
-            }else{
+            } else {
                 res.status(500).send({
                     status: false,
                     protocol: "err",
@@ -136,8 +140,71 @@ const PreguntaController = {
                 })
             }
         })
-    }
+    },
+    DevolverPreguntas: (req, res) => {
+        const { token } = req.body;
+        const a = new Auth();
+        
+        a.Admin(token, (respuesta) => {
+            if (respuesta.status) {
+                const p = new Pregunta();
+                p.devolverPreguntas((preguntas) => {
+                    if (preguntas.status) {
+                        res.status(200).send({
+                            status: true,
+                            protocol: "success",
+                            data: preguntas.data
+                        })
+                    } else {
+                        res.status(300).send({
+                            status: false,
+                            protocol: "err",
+                            message: preguntas.message
+                        })
+                    }
+                })
+            } else {
+                res.status(300).send({
+                    status: false,
+                    protocol: "err",
+                    message: "Token incorrecto"
+                });
+            }
 
+        })
+    },
+    PreguntaPorID: (req, res) => {
+        const {token, id} = req.body;
+        const a = new Auth();
+        
+        a.Admin(token, (respuesta) => {
+            if (respuesta.status) {
+                const p = new Pregunta();
+                p.getPreguntaPorId(id,(pregunta) => {
+                    if (pregunta.status) {
+                        res.status(200).send({
+                            status: true,
+                            protocol: "success",
+                            data: pregunta.data
+                        })
+                    } else {
+                        res.status(300).send({
+                            status: false,
+                            protocol: "err",
+                            message: pregunta.message
+                        })
+                    }
+                })
+            } else {
+                res.status(300).send({
+                    status: false,
+                    protocol: "err",
+                    message: "Token incorrecto"
+                });
+            }
+
+        })
+    }
 }
 
 export default PreguntaController;
